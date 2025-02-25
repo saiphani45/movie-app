ebbb383adebe0b35f905e70e3e74c395d2a8658d
Kotipalli Sai Phani Teja <donsai731@gmail.com>
2025-02-25 11:36:51 +0530

    added env configuration to the app

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,17 @@
-const API_KEY = "";
 const BASE_URL = "https://api.themoviedb.org/3";
+
+const apiKey = import.meta.env.VITE_API_KEY;
 
 
 export const getPopularMovies = async () => {
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${apiKey}`);
     const data = await response.json();
     return data.results;
 }
 
 export const searchMovies = async (query) => {
     console.log("query", query)
-    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}`);
     const data = await response.json();
     return data.results;
 }
